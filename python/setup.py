a1947cba0a1dd8053ca605e677aabfccb12765b3
runopti <>
2020-07-21 09:31:31 +0900

    update examples; add pip dependencies

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='stg',
-      version='0.1.0',
+      version='0.1.1',
       description='feature selection using stochastic gates',
       url='https://github.com/runopti/stg',
       author='Yutaro Yamada',
       author_email='yutaro.yamada@yale.edu',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='MIT',
       packages=['stg'],
+      install_requires=[
+          'torch',
+          'h5py',
+          'six',
+          'lifelines'
+      ],
       zip_safe=False)
